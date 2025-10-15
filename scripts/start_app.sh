270c79be7aea9c09142534b86a99c88d06331bd7
MACBOOK <macbook@macbook-pro.home>
2025-10-15 08:05:02 -0700

    feat: update

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,21 +1,20 @@
-#!/usr/bin/bash 
+#!/usr/bin/bash
 
-sed -i 's/\[]/\["13.48.149.227"]/' /home/ubuntu/project-space/aws_cicd/settings.py
+# Se placer dans le bon répertoire
+cd /home/ubuntu/project-space/aws_cicd
 
-python manage.py migrate 
-python manage.py makemigrations     
-python manage.py collectstatic
-sudo service gunicorn restart
-sudo service nginx restart
-#sudo tail -f /var/log/nginx/error.log
-#sudo systemctl reload nginx
-#sudo tail -f /var/log/nginx/error.log
-#sudo nginx -t
-#sudo systemctl restart gunicorn
-#sudo systemctl status gunicorn
-#sudo systemctl status nginx
-# Check the status
-#systemctl status gunicorn
-# Restart:
-#systemctl restart gunicorn
-#sudo systemctl status nginx
+# Modifier settings.py
+sed -i 's/\[]/\["13.48.149.227"]/' settings.py
+
+# Django Migrations
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic --noinput
+
+# Redémarrage des services
+sudo systemctl restart gunicorn
+sudo systemctl restart nginx
+
+# Vérification du statut
+sudo systemctl status gunicorn
+sudo systemctl status nginx
